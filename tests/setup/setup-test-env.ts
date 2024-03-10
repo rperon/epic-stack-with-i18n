ab1baba79bfab51cce38aff796c900d1e6da8071
Rémi Peron <remi.peron@gmail.com>
2024-03-10 18:25:09 +0100

    E2E Example

--- tests/setup/setup-test-env.ts
+++ tests/setup/setup-test-env.ts
@@ -5,7 +5,10 @@ import '#app/utils/env.server.ts'
 
 import { installGlobals } from '@remix-run/node'
 import { cleanup } from '@testing-library/react'
+import i18next from 'i18next'
+import { initReactI18next } from 'react-i18next'
 import { afterEach, beforeEach, vi, type SpyInstance } from 'vitest'
+import { i18n } from '#app/utils/i18n.ts'
 import { server } from '#tests/mocks/index.ts'
 import './custom-matchers.ts'
 
@@ -15,6 +18,10 @@ afterEach(() => server.resetHandlers())
 afterEach(() => cleanup())
 
 export let consoleError: SpyInstance<Parameters<(typeof console)['error']>>
+
+// setup-test-env.ts
+export const i18nextInstance = i18next.createInstance()
+await i18next.use(initReactI18next).init({ ns: ['en'], ...i18n })
 
 beforeEach(() => {
 	const originalConsoleError = console.error
